c9fd181f11a58daaf5c7abb3d29657de71d674d7
cherpin00 <44306236+cherpin00@users.noreply.github.com>
2018-10-19 18:03:04 -0500

    "2nd Commit"

--- howdy.py
+++ howdy.py
@@ -1,3 +1,9 @@
+def year2Month(years)
+  return years * 12
+
 print('Howdy, what is your name')
 a = input()
 print('well howdy,', a)
+
+myYears = input('how old are you')
+print("wow you have been alive for", year2Month(myYears), "years!")
